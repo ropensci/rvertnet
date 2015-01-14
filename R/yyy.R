66ddbc8dcf4066f45e47992606d86d78a1d9876a
Scott Chamberlain <myrmecocystus@gmail.com>
2015-01-14 11:28:24 -0800

    fixed vertsummary, pulled out print methods to a print function on the vertsummary class

--- R/yyy.R
+++ R/yyy.R
@@ -1,80 +1,90 @@
 # Wrapper for vertsummary function
-
-vertsumwrapper <- function(input = NULL)
+vertsumwrapper <- function(input = NULL, verbose = TRUE){
   
-{
-
   if(is(input, "list"))  input <- ldply(input, data.frame)
-
+  
   # recs <- number of records in the data frame
   recs <- nrow(input)
-	cat("\n\nNumber of records ($recs): ", recs, sep = "")
-
+  
   # coords <- number of records with viable lat and long data
   # errest <- number of "coords" records with viable coordinate uncertainty estimate
-  if(inherits(input$decimallatitude, "NULL") & 
-    inherits(input$decimallongitude, "NULL")){coords <- 0} else{coords <- NULL}
-  if(inherits(input$coordinateuncertaintyinmeters, "NULL")){errest <- 0} else{errest <- NULL}
+  if(is.null(input$decimallatitude) & is.null(input$decimallongitude)){
+    coords <- 0
+  } else{ 
+    coords <- NULL
+  }
+  if(inherits(input$coordinateuncertaintyinmeters, "NULL")){
+    errest <- 0
+  } else {
+    errest <- NULL
+  }
   if(is.null(coords)){
-    coords <- sum(complete.cases(input[,list(input$decimallatitude, 
-                                             input$decimallongitude)]))
+    coords <- sum(complete.cases(input[,c('decimallatitude','decimallongitude')]))
     # checking for good lat/long data (if not, use only the above line)
     input$decimallatitude <- as.numeric(as.character(input$decimallatitude))
     input$decimallongitude <- as.numeric(as.character(input$decimallongitude))
-    if(is.null(errest)){input$coordinateuncertaintyinmeters <-
-      as.numeric(as.character(input$coordinateuncertaintyinmeters))}
-    mappable <- input[complete.cases(input[,list(input$decimallatitude, 
-                                                 input$decimallongitude)]),]
+    if(is.null(errest)){
+      input$coordinateuncertaintyinmeters <- as.numeric(as.character(input$coordinateuncertaintyinmeters))
+    }
+    mappable <- input[complete.cases(input[,c('decimallatitude','decimallongitude')]),]
     mappable <- subset(mappable, input$decimallatitude < 90 & input$decimallatitude > -90)
     mappable <- subset(mappable, input$decimallongitude < 180 & input$decimallongitude > -180)
     if(nrow(mappable) < coords){
       bad <- coords - nrow(mappable)
-      message(bad, " record(s) with bad coordinates")
+      mssg(verbose, paste(bad, " record(s) with bad coordinates"))
       coords <- coords - bad
     }
     if(is.null(errest)){
       mappable <- subset(mappable, input$coordinateuncertaintyinmeters > 0 &
-                         input$coordinateuncertaintyinmeters < 20020000)
-      if((errest <- nrow(mappable)) < coords){bad <- coords - errest}
+                           input$coordinateuncertaintyinmeters < 20020000)
+      if((errest <- nrow(mappable)) < coords){
+        bad <- coords - errest
+      }
     }
   }
-  cat("\n\nRecords with decimal lat/long ($coords): ", coords, sep = "")
-  message("(-90<lat<90, -180<long<180)")
-  cat("\n\nRecords with lat/long and coordinate uncertainty estimate ($errest): ", errest)
-  message("(0<errest<20020000)")
-
+  
   # instcoll <- number of records from each institution+collection
-  removeDups <- function(x) {paste(unique(unlist(strsplit(x, split=" "))), collapse = " ")}
-  if(inherits(input$institutioncode, "NULL") & inherits(input$collectioncode, "NULL")){
-    instcoll <- NA} else{
-      instcoll <- as.matrix(paste(input[,input$institutioncode], 
-                                  input[,input$collectioncode], sep = " "))
-      instcoll <- table(apply(instcoll, 1, removeDups))
+  removeDups <- function(x) {
+    paste(unique(unlist(strsplit(x, split=" "))), collapse = " ")
   }
-  cat("\n\nRecord count by institution/collection ($instcoll): ")
-  print(instcoll)
+  if(inherits(input$institutioncode, "NULL") & inherits(input$collectioncode, "NULL")){
+    instcoll <- NA
+  } else {
+    instcoll <- as.matrix(paste(input[,'institutioncode'], 
+                                input[,'collectioncode'], sep = " "))
+    instcoll <- table(apply(instcoll, 1, removeDups))
+  }
   
   # country <- number of records from each country
   if(inherits(input$country, "NULL")){country <- NA} else{country <- table(input$country)}
-  cat("\n\nRecord count by country ($country): ")
-  print(country)
-
+  
   # year <- number of records by year
   if(inherits(input$year, "NULL")){year <- NA} else{year <- table(input$year)}
-  cat("\n\nRecord count by year ($year): ")
-  print(year)
-
+  
   # taxon <- number of records by taxonomic name
-  taxon <- as.matrix(paste(input[,input$genus], input[,input$specificepithet], sep = " "))
+  taxon <- as.matrix(paste(input[,'genus'], input[,'specificepithet'], sep = " "))
   if(!inherits(input$infraspecificepithet, "NULL")){
-    taxon <- as.matrix(paste(taxon, input[,input$infraspecificepithet], sep = " "))}
+    taxon <- as.matrix(paste(taxon, input[,'infraspecificepithet'], sep = " "))
+  }
   taxon <- gsub(" NA", "", taxon) # remove unknowns - usually infrasp.ep
   taxon <- table(apply(taxon, 1, removeDups))
-  cat("\n\nRecord count by taxon ($taxon): ")
-  print(taxon)
   
   # return summary
-  s <- list("recs" = recs, "coords" = coords, "errest" = errest, "instcoll" = instcoll,
-            "country" = country, "year" = year, "taxon" = taxon)
+  structure(list("recs" = recs, "coords" = coords, "errest" = errest, "instcoll" = instcoll,
+                 "country" = country, "year" = year, "taxon" = taxon), class="vertsummary")
+}
 
+#' @export
+print.vertsummary <- function(x, ...){
+  cat(paste0("Number of records ($recs): ", x$recs), sep = "\n") 
+  cat(paste("Records with decimal lat/long (-90<lat<90, -180<long<180) ($coords): ", x$coords, sep = ""), sep = "\n")
+  cat(paste("Records with lat/long and coordinate uncertainty estimate (0<errest<20020000) ($errest): ", x$errest), sep = "\n")
+  cat("Record count by institution/collection ($instcoll): ", sep = "\n")
+  print(x$instcoll)
+  cat("\nRecord count by country ($country): ", sep = "\n")
+  print(x$country)
+  cat("\nRecord count by year ($year): ", sep = "\n")
+  print(x$year)
+  cat("\nRecord count by taxon ($taxon): ", sep = "\n")
+  print(x$taxon) 
 }
