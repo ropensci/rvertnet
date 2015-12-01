b0d1cd7e5a4a4734df76f0a93f225864432e6bef
Scott Chamberlain <myrmecocystus@gmail.com>
2015-11-30 17:52:36 -0800

    added a few more tets, bump version

--- tests/testthat/test-vertmap.R
+++ tests/testthat/test-vertmap.R
@@ -13,6 +13,20 @@ test_that("vertmap works with vertsearch", {
   expect_is(map1$mapping$x, "name")
 })
 
+test_that("vertmap works for maps not distinguished by color", {
+  skip_on_cran()
+  
+  out <- vertsearch("mustela nigripes", verbose = FALSE, limit = 50)
+  out$data$scientificname <- NULL
+  map1 <- vertmap(input = out, mapdatabase = "state")
+  
+  expect_is(map1, "gg")
+  expect_is(map1$data, "data.frame")
+  expect_null(map1$labels$colour) # the difference here
+  expect_equal(as.character(map1$mapping$x), "long")
+  expect_is(map1$mapping$x, "name")
+})
+
 test_that("vertmap fails well", {
   skip_on_cran()
   
