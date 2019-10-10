060bc6e5cb1c1cb67c9df8ebba11aefa0ade627a
Scott Chamberlain <myrmecocystus@gmail.com>
2019-10-10 11:51:28 -0700

    fix assert() fxn to use !inherits() instead of x in y

--- R/zzz.R
+++ R/zzz.R
@@ -174,7 +174,7 @@ rvc <- function(x) Filter(Negate(is.null), x)
 
 assert <- function(x, y) {
   if (!is.null(x)) {
-    if (!class(x) %in% y) {
+    if (!inherits(x, y)) {
       stop(deparse(substitute(x)), " must be of class ",
            paste0(y, collapse = ", "), call. = FALSE)
     }
